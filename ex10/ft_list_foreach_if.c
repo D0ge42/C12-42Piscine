b2ec1cb564b5225844b0a77a733401562f439f20
Doge42Casa <midnatwitch@gmail.com>
2024-08-26 23:11:55 +0200

    ex10

---
+++ ex10/ft_list_foreach_if.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach_if.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lonulli <lonulli@student.42roma.it>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 22:58:26 by lonulli           #+#    #+#             */
+/*   Updated: 2024/08/26 23:01:17 by lonulli          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach_if(t_list *begin_list, void (*f)(void *), void
+*data_ref, int (*cmp)())
+{
+	while (begin_list)
+	{
+		if ((*cmp)(begin_list->data, data_ref) == 0)
+			(*f)(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
