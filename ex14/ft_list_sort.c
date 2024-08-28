ae7df72bad11a7837852dd9bdc2bed8d25bdbe50
Doge42Casa <midnatwitch@gmail.com>
2024-08-28 15:35:53 +0200

    Fixed ex14, had to reset to beginning each time inner loop was completed

--- ex14/ft_list_sort.c
+++ ex14/ft_list_sort.c
@@ -13,17 +13,19 @@
 #include "ft_list.h"
 #include <stdio.h>
 #include <string.h>
-//Dopo un loop completo va avanti a prescindere e non controlla
-//se il primo è quello più piccolo.
+
 void	ft_list_sort(t_list **begin_list, int (*cmp)())
 {
-	t_list	*point = *begin_list;
-	void 	*tmp;
-	t_list *orig_point = point;
-	while(orig_point)
+	t_list	*point;
+	void	*tmp;
+	t_list	*orig_point;
+
+	point = *begin_list;
+	orig_point = *begin_list;
+	while (orig_point)
 	{
-		point = orig_point;
-		while(point->next)
+		point = *begin_list;
+		while (point->next)
 		{
 			if ((*cmp)(point->data, point->next->data) > 0)
 			{
@@ -31,13 +33,13 @@ void	ft_list_sort(t_list **begin_list, int (*cmp)())
 				point->data = point->next->data;
 				point->next->data = tmp;
 			}
-			 point = point->next;
+			point = point->next;
 		}
 		orig_point = orig_point->next;
 	}
 }
 
-int cmp(void *a, void *b)
+/*int cmp(void *a, void *b)
 {
 	return strcmp ((char *)a, (char *)b);
 }
@@ -52,6 +54,18 @@ void ft_print_list(t_list *begin_list)
 	printf("NULL\n");
 }
 
+void free_list(t_list *head)
+{
+	t_list	*tmp;
+	
+	while(head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
 int main()
 {
 	t_list *node1, *node2, *node3, *node4, *node5;
@@ -63,22 +77,24 @@ int main()
 	node4 = (t_list *)malloc(sizeof(t_list));
 	node5 = (t_list *)malloc(sizeof(t_list));
 
-	node1->data = "ciao4";
+	node1->data = "ciao3";
 	node1->next = node2;
 
 	node2->data = "ciao2";
 	node2->next = node3;
 
-	node3->data = "ciao3";
+	node3->data = "ciao0";
 	node3->next =  node4;
 
 	node4->data = "ciao1";
 	node4->next = node5;
 
-	node5->data = "ciao1";
+	node5->data = "ciao2";
 	node5->next = NULL;
 	
 	ft_print_list(node1);
 	ft_list_sort(&node1,cmp);
 	ft_print_list(node1);
-}
+
+	free_list(node1);
+}*/
