1062bcf6774c2cc1d841a53ec8dabd320dccf902
Doge42Casa <midnatwitch@gmail.com>
2024-08-28 14:59:43 +0200

    trying to fix ex14

--- ex14/ft_list_sort.c
+++ ex14/ft_list_sort.c
@@ -11,26 +11,19 @@
 /* ************************************************************************** */
 
 #include "ft_list.h"
-
 #include <stdio.h>
 #include <string.h>
+//Dopo un loop completo va avanti a prescindere e non controlla
+//se il primo è quello più piccolo.
 void	ft_list_sort(t_list **begin_list, int (*cmp)())
 {
 	t_list	*point = *begin_list;
-	int i = 0;
 	void 	*tmp;
-	while(point)
+	t_list *orig_point = point;
+	while(orig_point)
 	{
-		i++;
-		point = point->next;
-	}
-	point = *begin_list;
-	int count = i;
-	i = 0;
-	int j = 0;
-	while(i < count)
-	{	
-		while(j < count - 1)
+		point = orig_point;
+		while(point->next)
 		{
 			if ((*cmp)(point->data, point->next->data) > 0)
 			{
@@ -38,10 +31,9 @@ void	ft_list_sort(t_list **begin_list, int (*cmp)())
 				point->data = point->next->data;
 				point->next->data = tmp;
 			}
-			point = point->next;
-			j++;
+			 point = point->next;
 		}
-		i++;
+		orig_point = orig_point->next;
 	}
 }
 
@@ -62,27 +54,30 @@ void ft_print_list(t_list *begin_list)
 
 int main()
 {
-	t_list *node1, *node2, *node3, *node4;
+	t_list *node1, *node2, *node3, *node4, *node5;
 
 
 	node1 = (t_list *)malloc(sizeof(t_list));
 	node2 = (t_list *)malloc(sizeof(t_list));
 	node3 = (t_list *)malloc(sizeof(t_list));	
 	node4 = (t_list *)malloc(sizeof(t_list));
+	node5 = (t_list *)malloc(sizeof(t_list));
 
-	node1->data = "ciao9";
+	node1->data = "ciao4";
 	node1->next = node2;
 
-	node2->data = "ciao9";
+	node2->data = "ciao2";
 	node2->next = node3;
 
-	node3->data = "ciao2";
+	node3->data = "ciao3";
 	node3->next =  node4;
 
-	node4->data = "ciao3";
-	node4->next = NULL;
+	node4->data = "ciao1";
+	node4->next = node5;
 
-
+	node5->data = "ciao1";
+	node5->next = NULL;
+	
 	ft_print_list(node1);
 	ft_list_sort(&node1,cmp);
 	ft_print_list(node1);
