c7d709cb4e6a7b31b026b6f8c57a8c57da2e75c5
Doge42Casa <midnatwitch@gmail.com>
2024-08-29 13:05:19 +0200

    Added ex16 but need fixes

---
+++ ex16/ft_sorted_list_insert.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sorted_list_insert.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lonulli <lonulli@student.42roma.it>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/29 11:39:11 by lonulli           #+#    #+#             */
+/*   Updated: 2024/08/29 11:39:13 by lonulli          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stdio.h>
+#include <string.h>
+
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->data = data;
+	new->next = NULL;
+	return (new);
+}
+//Il nodo più piccolo scompare? Perchè?
+void  ft_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)())
+ {
+	t_list *start, *end, *og;
+	void *temp;
+
+	t_list *new = ft_create_elem(data);
+	start = *begin_list;
+	og = *begin_list;
+	while(og)
+	{
+		start = *begin_list;
+		while(start->next)
+		{
+			if((*cmp)(start->data, start->next->data) > 0)
+			{
+				temp = start->data;
+				start->data = start->next->data;
+				start->next->data = temp;
+			}
+			start = start->next;
+		}
+		og = og->next;
+	}	
+}
+
+int cmp(void *a, void *b)
+{
+	return strcmp ((char *)a, (char *)b);
+}
+
+void ft_print_list(t_list *begin_list)
+{
+	while(begin_list)
+	{
+		printf("%s ->",(char *)begin_list->data);
+		begin_list = begin_list->next;
+	}
+	printf("NULL\n");
+}
+
+int main()
+{
+	t_list *node1, *node2, *node3, *node4, *node5;
+
+
+	node1 = (t_list *)malloc(sizeof(t_list));
+	node2 = (t_list *)malloc(sizeof(t_list));
+	node3 = (t_list *)malloc(sizeof(t_list));	
+	node4 = (t_list *)malloc(sizeof(t_list));
+
+	node1->data = "3";
+	node1->next = node2;
+
+	node2->data = "2";
+	node2->next = node3;
+
+	node3->data = "7";
+	node3->next =  node4;
+
+	node4->data = "1";
+	node4->next = NULL;
+
+	ft_print_list(node1);
+	ft_sorted_list_insert(&node1, "6" , cmp);
+	ft_print_list(node1);
+
+}
